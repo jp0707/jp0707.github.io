a27446f04003e0b0d6f161f23eb7a66b180b0dd7
Jyoti Patel <jyotipatel152@gmail.com>
2020-08-04 10:44:24 -0400

    Change video URL

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -14,12 +14,12 @@ class Portfolio extends Component {
                 <br/>Functionalities implemented were voting, comment, share, infinite scrolling, stop infinite retrying on error, etc.
               </p>
               <h3>Hacker News App using React Native</h3>
-              <p><a href="https://www.youtube.com/watch?v=MU4chvrRHHQ">Check it live here</a>
+              <p><a href="https://youtu.be/woYhWfJ4cBo">Check it live here</a>
                 <br/><a href="https://github.com/jp0707/hn">Code</a>
                 <br/>Developed an App for which displays a list of news, each news displays the article and comments in different tabs.
                 <br/>Functionalities implemented were infinite scrolling, pull to request, screen navigation, tab navigation, nested and collapsable comment view, decode html entities.
                 <br/><br/>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/MU4chvrRHHQ" title="hn" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/woYhWfJ4cBo" title="hn" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
               </p>
               <h3>Resume Website</h3>
               <p><a href="https://jyoti.dev">Check it live here</a>
